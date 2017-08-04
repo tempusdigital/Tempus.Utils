a14bb9544981f13f3c328f1c15cf40495ce850aa
Bruno Albano de Souza <brunoalbano@tempus.digital>
2017-08-04 14:14:23 -0300

    Add Server Side Validation

---
+++ Tempus.Extensions/ServerSideValidationException.cs
@@ -0,0 +1,17 @@
+﻿namespace Tempus.Utils
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	
+    public class ServerSideValidationException : Exception
+    {
+        public ServerSideValidationException() : base() { }
+
+        public ServerSideValidationException(string message) : base(message) { }
+
+        public ServerSideValidationException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
